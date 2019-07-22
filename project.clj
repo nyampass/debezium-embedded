d70af95480e68ab5bdfdd0ac16f39f965d7d0203
Tokusei Noborio <tokuse@nyampass.com>
2019-07-22 09:47:28 +0900

    Downgrade io.debezium/debezium-embedded

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject hden/debezium-embedded "0.2.0-SNAPSHOT"
+(defproject nyampass/debezium-embedded "0.2.1-SNAPSHOT"
   :description "A Clojure wrapper for the debezium-embedded engine."
   :url "http://example.com/FIXME"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.debezium/debezium-embedded "0.10.0.Beta2"]]
+                 [io.debezium/debezium-embedded "0.9.5.Final"]]
   :repl-options {:init-ns debezium-embedded.core})
